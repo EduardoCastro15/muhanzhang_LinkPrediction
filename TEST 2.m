7ea3171b18673c7b84c1b60c64dd012938515867
Jorge Eduardo Castro Cruces <acw792@qmul.ac.uk>
2024-10-28 17:04:23 +0000

    Removed duplicate files and updated .DS_Store modifications

--- TEST 2.m
+++
@@ -1,7 +0,0 @@
-data = load('data/foodweb_sparse.mat');
-whos -file data/foodweb_sparse.mat
-
-% data.net  % Replace 'variable_name' with the actual variable name
-
-data = load('data/USAir.mat');
-whos -file data/USAir.mat
