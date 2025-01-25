8aee97111b53747cb3b937aa57c78f4b8c7083ba
Jorge Eduardo Castro Cruces <acw792@qmul.ac.uk>
2025-01-25 13:18:56 +0000

    I review the correect coexistance of both modifications and adjust indentation #3

--- utils/DivideNet.m
+++ utils/DivideNet.m
@@ -10,94 +10,94 @@ function [train, test ] = DivideNet(net, ratioTrain)
 %%  *problem identified: duplicate test links due to non-triangular matrix
 %  This function has been updated to support directed graphs without enforcing symmetry.
 
-% Debugging: Check if the input graph is symmetric
-disp('Debug: Checking input graph for symmetry at the start of (DivideNet.m)...');
-if isequal(net, net')
-    disp('Warning: Input graph is undirected (symmetric adjacency matrix).');
-else
-    disp('Debug: Input graph is directed.');
-end
-
-% Remove self-loops (if any)
-net = net - diag(diag(net));
-
-% Debugging: Check if self-loops were correctly removed
-disp(['Debug: Number of self-loops removed: ', num2str(sum(diag(net) ~= 0))]);
-
-% Calculate the number of edges for the test set
-num_testlinks = ceil((1-ratioTrain) * nnz(net));
-
-% Identify all edges in the network and store them in linklist
-[xindex, yindex] = find(net);
-linklist = [xindex yindex];
-
-clear xindex yindex;  % Remove unnecessary variables
-
-% Initialize the test set as a sparse matrix of the same size as net
-test = sparse(size(net,1), size(net,2));
-
-% Randomly select edges to add to the test set until the desired count is reached
-while (nnz(test) < num_testlinks)
-    if length(linklist) <= 2
-        break;
+    % Debugging: Check if the input graph is symmetric
+    disp('Debug: Checking input graph for symmetry at the start of (DivideNet.m)...');
+    if isequal(net, net')
+        disp('Warning: Input graph is undirected (symmetric adjacency matrix).');
+    else
+        disp('Debug: Input graph is directed.');
     end
 
-    % Randomly choose an edge from the link list
-    index_link = ceil(rand(1) * length(linklist));
+    % Remove self-loops (if any)
+    net = net - diag(diag(net));
 
-    % Identify the nodes connected by the selected edge
-    uid1 = linklist(index_link, 1);
-    uid2 = linklist(index_link, 2);
-    net(uid1, uid2) = 0;  % Temporarily remove the edge from the network
+    % Debugging: Check if self-loops were correctly removed
+    disp(['Debug: Number of self-loops removed: ', num2str(sum(diag(net) ~= 0))]);
 
-    %% Check if nodes uid1 and uid2 remain reachable
-    tempvector = net(uid1, :);  % Get nodes reachable from uid1 in one step
-    reachable = 0;  % Default: edge cannot be removed
+    % Calculate the number of edges for the test set
+    num_testlinks = ceil((1-ratioTrain) * nnz(net));
 
-    % Calculate reachability within two steps
-    uid1TOuid2 = tempvector * net + tempvector;
-    if uid1TOuid2(uid2) > 0
-        reachable = 1;  % Mark as reachable
-    else
-        % Check reachability for more than two steps until stable
-        while (nnz(spones(uid1TOuid2) - tempvector) ~= 0)
-            tempvector = spones(uid1TOuid2);
-            uid1TOuid2 = tempvector * net + tempvector;
-            if uid1TOuid2(uid2) > 0
-                reachable = 1;  % Mark as reachable
-                break;
+    % Identify all edges in the network and store them in linklist
+    [xindex, yindex] = find(net);
+    linklist = [xindex yindex];
+
+    clear xindex yindex;  % Remove unnecessary variables
+
+    % Initialize the test set as a sparse matrix of the same size as net
+    test = sparse(size(net,1), size(net,2));
+
+    % Randomly select edges to add to the test set until the desired count is reached
+    while (nnz(test) < num_testlinks)
+        if length(linklist) <= 2
+            break;
+        end
+
+        % Randomly choose an edge from the link list
+        index_link = ceil(rand(1) * length(linklist));
+
+        % Identify the nodes connected by the selected edge
+        uid1 = linklist(index_link, 1);
+        uid2 = linklist(index_link, 2);
+        net(uid1, uid2) = 0;  % Temporarily remove the edge from the network
+
+        %% Check if nodes uid1 and uid2 remain reachable
+        tempvector = net(uid1, :);  % Get nodes reachable from uid1 in one step
+        reachable = 0;  % Default: edge cannot be removed
+
+        % Calculate reachability within two steps
+        uid1TOuid2 = tempvector * net + tempvector;
+        if uid1TOuid2(uid2) > 0
+            reachable = 1;  % Mark as reachable
+        else
+            % Check reachability for more than two steps until stable
+            while (nnz(spones(uid1TOuid2) - tempvector) ~= 0)
+                tempvector = spones(uid1TOuid2);
+                uid1TOuid2 = tempvector * net + tempvector;
+                if uid1TOuid2(uid2) > 0
+                    reachable = 1;  % Mark as reachable
+                    break;
+                end
             end
+        end
+
+        % Modified: Allow all selected edges in the test set, regardless of connectivity
+        reachable = 1;
+
+        %% Add edge to the test set or restore it in the training network
+        if reachable == 1  % Edge can be deleted
+            linklist(index_link, :) = [];  % Remove edge from linklist
+            test(uid1, uid2) = 1;  % Mark as test edge
+        else
+            net(uid1, uid2) = 1;  % Restore edge in the network
         end
     end
 
-    % Modified: Allow all selected edges in the test set, regardless of connectivity
-    reachable = 1;
+    % Generate the symmetric training and testing adjacency matrices
+    train = net;
 
-    %% Add edge to the test set or restore it in the training network
-    if reachable == 1  % Edge can be deleted
-        linklist(index_link, :) = [];  % Remove edge from linklist
-        test(uid1, uid2) = 1;  % Mark as test edge
+    % Debugging: Check if the training graph is symmetric
+    disp('Debug: Checking training graph for symmetry (DivideNet.m)...');
+    if isequal(train, train')
+        disp('Warning: Training graph is undirected (symmetric adjacency matrix).');
     else
-        net(uid1, uid2) = 1;  % Restore edge in the network
+        disp('Debug: Training graph is directed.');
     end
-end
 
-% Generate the symmetric training and testing adjacency matrices
-train = net;
-
-% Debugging: Check if the training graph is symmetric
-disp('Debug: Checking training graph for symmetry (DivideNet.m)...');
-if isequal(train, train')
-    disp('Warning: Training graph is undirected (symmetric adjacency matrix).');
-else
-    disp('Debug: Training graph is directed.');
-end
-
-% Debugging: Check if the test graph is symmetric
-disp('Debug: Checking testing graph for symmetry (DivideNet.m)...');
-if isequal(test, test')
-    disp('Warning: Testing graph is undirected (symmetric adjacency matrix).');
-else
-    disp('Debug: Testing graph is directed.');
-end
+    % Debugging: Check if the test graph is symmetric
+    disp('Debug: Checking testing graph for symmetry (DivideNet.m)...');
+    if isequal(test, test')
+        disp('Warning: Testing graph is undirected (symmetric adjacency matrix).');
+    else
+        disp('Debug: Testing graph is directed.');
+    end
 end
